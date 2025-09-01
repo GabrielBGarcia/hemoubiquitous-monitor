617f6661494ca455ad63e5d9bfabd8746ccd5e8c
João Vitor <joaovitoralves2009@hotmail.com>
2025-09-01 19:57:03 -0300

    backend initial commit

---
+++ backend/src/main/java/com/ufg/hemoubiquitous_monitor/HemoubiquitousMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ufg.hemoubiquitous_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemoubiquitousMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HemoubiquitousMonitorApplication.class, args);
+	}
+
+}
